70788a967b1f8fbe52f60babad29b9b5f9d32ff0
hamletfg <hamletfg@gmail.com>
2025-07-09 09:06:47 -0600

    Initial capstone repo setup

---
+++ server/server.js
@@ -0,0 +1,11 @@
+// server/server.js
+
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
