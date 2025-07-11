e783877073143aba7e9c677975a229bb31676d85
Miriam Rawlins <miriammrawlins@gmail.com>
2025-07-11 09:28:14 -0600

    adding winston and morgan

--- server/server.js
+++ server/server.js
@@ -1,10 +1,33 @@
 // server/server.js
 
-const express = require('express');
+const express = require("express");
+const morgan = require("morgan");
+const winston = require("winston");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+const logger = winston.createLogger({
+  level: "http",
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: "logfile.log" }),
+  ],
+});
+
+app.use(
+  morgan("combined", {
+    stream: {
+      write: (message) => logger.http(message.trim()),
+    },
+  })
+);
+
+app.get("/", (req, res) => {
+  logger.info("Home page accessed");
+  res.send("Hello, Winston and Morgan");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
