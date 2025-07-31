1efa8f3972ae4c01c971af8dfe2f39e3e23d51b7
Hamlet Gordillo <hamletfg@gmail.com>
2025-07-30 21:46:17 -0600

    Change to pool to get server working on render

--- server/db/db.js
+++ server/db/db.js
@@ -1,20 +1,19 @@
-const { Pool } = require("pg");
-require("dotenv").config();
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_DATABASE,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Test the connection
-pool.query("SELECT NOW()", (err, res) => {
+pool.query('SELECT NOW()', (err, res) => {
   if (err) {
-    console.error("Error connecting to the database:", err);
+    console.error('Error connecting to the database:', err);
   } else {
-    console.log("Successfully connected to PostgreSQL database");
+    console.log('Successfully connected to PostgreSQL database');
   }
 });
 
