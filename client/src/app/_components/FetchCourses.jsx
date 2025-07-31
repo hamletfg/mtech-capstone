cf8a30901e0cbaee6022413dd5e1492f6e23911a
Hamlet Gordillo <hamletfg@gmail.com>
2025-07-30 18:11:53 -0600

    Render deploy setup

--- client/src/app/_components/FetchCourses.jsx
+++ client/src/app/_components/FetchCourses.jsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import Link from "next/link";
+import { useEffect, useState } from 'react';
+import Link from 'next/link';
 
 export default function FetchCourses({ token }) {
   const [course, setCourses] = useState([]);
@@ -11,20 +11,20 @@ export default function FetchCourses({ token }) {
   useEffect(() => {
     async function fetchCourses() {
       if (!token) {
-        throw new Error("No authentication token provided");
+        throw new Error('No authentication token provided');
       }
-      const res = await fetch("http://localhost:3001/api/courses", {
-        method: "GET",
+      const res = await fetch('/api/courses', {
+        method: 'GET',
         headers: {
           Authorization: `Bearer ${token}`,
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
       });
       console.log(res);
 
       try {
         if (!res.ok) {
-          throw new Error("Failed to fetch course data");
+          throw new Error('Failed to fetch course data');
         }
         const data = await res.json();
         setCourses(data);
@@ -37,21 +37,21 @@ export default function FetchCourses({ token }) {
   }, [token]);
 
   return (
-    <div className="grid grid-cols-4 gap-8 p-10">
+    <div className='grid grid-cols-4 gap-8 p-10'>
       {course.map((course, index) => {
         return (
           <Link
             href={`/courses/${course.course_id}`}
             key={index}
-            className="border border-gray-500 p-5 rounded-sm overflow-hidden"
+            className='border border-gray-500 p-5 rounded-sm overflow-hidden'
           >
             <div>
-              <div className="mb-2">
+              <div className='mb-2'>
                 <span>{course.course_id}</span>
               </div>
-              <div className="mb-2">{course.title}</div>
+              <div className='mb-2'>{course.title}</div>
               <div>
-                <span className="text-sm  line-clamp-3">
+                <span className='text-sm  line-clamp-3'>
                   {course.description}
                 </span>
               </div>
--- client/src/app/join/page.jsx
+++ client/src/app/join/page.jsx
@@ -1,23 +1,23 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import Button from "../_components/Button";
-import Input from "../_components/Input";
-import { useRouter } from "next/navigation";
-import Link from "next/link";
+import { useState } from 'react';
+import Button from '../_components/Button';
+import Input from '../_components/Input';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
 
 export default function Page() {
   const [userData, setUserData] = useState({
-    email: "",
-    password: "",
-    firstName: "",
-    lastName: "",
-    telephone: "",
-    street: "",
-    city: "",
-    state: "",
-    zip_code: "",
-    country: "",
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    telephone: '',
+    street: '',
+    city: '',
+    state: '',
+    zip_code: '',
+    country: '',
   });
 
   const handleInputChange = (e) => {
@@ -32,10 +32,10 @@ export default function Page() {
     console.log(userData);
 
     try {
-      const res = await fetch("http://localhost:3001/api/join", {
-        method: "POST",
+      const res = await fetch('/api/join', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
         body: JSON.stringify(userData),
       });
@@ -43,95 +43,95 @@ export default function Page() {
       console.log(res);
       if (res.ok) {
         const result = await res.json();
-        console.log("Success", result);
+        console.log('Success', result);
       } else {
-        console.log("Error", res.status);
+        console.log('Error', res.status);
       }
     } catch (err) {
-      console.log("Network error", err);
+      console.log('Network error', err);
     }
   };
 
   return (
-    <div className="p-5">
+    <div className='p-5'>
       <form
         onSubmit={handleJoin}
-        className="flex flex-col justify-center items-center gap-5 mx-auto"
+        className='flex flex-col justify-center items-center gap-5 mx-auto'
       >
         <Input
-          type="email"
-          id="email"
-          name="email"
-          placeholder="email@example.com"
+          type='email'
+          id='email'
+          name='email'
+          placeholder='email@example.com'
           onChange={handleInputChange}
         />
         <Input
-          type="password"
-          id="password"
-          name="password"
-          placeholder="password"
+          type='password'
+          id='password'
+          name='password'
+          placeholder='password'
           onChange={handleInputChange}
         />
         <Input
-          type="text"
-          id="firstName"
-          name="firstName"
-          placeholder="First Name"
+          type='text'
+          id='firstName'
+          name='firstName'
+          placeholder='First Name'
           onChange={handleInputChange}
         />
         <Input
-          type="text"
-          id="lastName"
-          name="lastName"
-          placeholder="Last Name"
+          type='text'
+          id='lastName'
+          name='lastName'
+          placeholder='Last Name'
           onChange={handleInputChange}
         />
         <Input
-          type="tel"
-          id="telephone"
-          name="telephone"
-          placeholder="0001112222"
+          type='tel'
+          id='telephone'
+          name='telephone'
+          placeholder='0001112222'
           onChange={handleInputChange}
         />
         <Input
-          type="text"
-          id="street"
-          name="street"
-          placeholder="123 Example Road"
+          type='text'
+          id='street'
+          name='street'
+          placeholder='123 Example Road'
           onChange={handleInputChange}
         />
         <Input
-          type="text"
-          id="city"
-          name="city"
-          placeholder="Example City"
+          type='text'
+          id='city'
+          name='city'
+          placeholder='Example City'
           onChange={handleInputChange}
         />
         <Input
-          type="text"
-          id="state"
-          name="state"
-          placeholder="NY"
+          type='text'
+          id='state'
+          name='state'
+          placeholder='NY'
           onChange={handleInputChange}
         />
         <Input
-          type="number"
-          id="zip_code"
-          name="zip_code"
-          placeholder="zip code"
+          type='number'
+          id='zip_code'
+          name='zip_code'
+          placeholder='zip code'
           onChange={handleInputChange}
         />
         <select
-          id="country"
-          name="country"
-          className="border py-2 px-4 rounded-lg"
+          id='country'
+          name='country'
+          className='border py-2 px-4 rounded-lg'
           onChange={handleInputChange}
         >
           <option>Select country</option>
           <option>USA</option>
         </select>
 
-        <Button type="submit">Join</Button>
+        <Button type='submit'>Join</Button>
       </form>
     </div>
   );
